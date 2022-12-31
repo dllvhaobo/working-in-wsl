d2e2a57986cfcb1e50ed2f6d286674f426f65448
dllvhaobo <dl_lvhaobo@163.com>
2022-12-31 20:12:57 +0800

    Update

--- docs/readme.md
+++ docs/readme.md
@@ -1,117 +1,5 @@
-<!-- vim-markdown-toc GFM -->
 
-* [QuickInstall](#quickinstall)
-* [Prepare](#prepare)
-* [dependency](#dependency)
-* [neovim](#neovim)
-    * [vim-plug](#vim-plug)
-    * [Nodejs](#nodejs)
-    * [Python](#python)
-    * [copy-plugin](#copy-plugin)
-    * [gtags](#gtags)
-    * [ctags](#ctags)
-    * [ripgrep](#ripgrep)
-* [Git](#git)
-* [ZSH](#zsh)
-* [关闭 PowerLevel9K 的检查 GIT 更新 Option](#关闭-powerlevel9k-的检查-git-更新-option)
-* [SSH 登录](#ssh-登录)
-
-<!-- vim-markdown-toc -->
-
-## QuickInstall
-
-QuickInstall 可以完成所有相关配置，可以忽略后续的章节。
-
-受到各种原因的限制，目前的开发环境只能是基于 UBUNTU-18.04.5 的环境。基于此目前可以使用以下三种方式搭建环境。
-
-- SSH 登录编译服务器，参考如下脚本进行服务器配置。
-
-  ```bash
-  git clone git@cnninvmgtlb01:lv_h1/syscfg.git
-  cd syscfg/non-root-install
-  ./quickinstall_1804_non_root.sh
-  ```
-
-- WSL2 + Ubuntu18.04，参考如下脚本进行服务器配置。
-
-  ```bash
-  git clone git@cnninvmgtlb01:lv_h1/syscfg.git
-  cd syscfg/non-root-install
-  ./quickinstall_1804_root.sh
-  ```
-
-- Docker + Ubuntu18.04 镜像，启动 docker 镜像，参考`docker_config.sh`配置用户；然后参考 WSL2 的配置脚本即可。
-
-## Prepare
-
-```bash
-mkdir -p ${XDG_DATA_HOME:-$HOME/.local}/share
-mkdir -p ${XDG_DATA_HOME:-$HOME/.local}/bin
-mkdir -p ${XDG_DATA_HOME:-$HOME/.local}/lib
-
-git clone git@cnninvmgtlb01:lv_h1/syscfg.git`
-```
-
-## dependency
-
-目前服务器所有依赖已装备，可跳过。
-
-```bash
-sudo apt install git make g++ gcc clang-12 automake build-essential python3-pip cmake automake autoconf libncurses5-dev libdist-zilla-plugin-localemsgfmt-perl liblocale-msgfmt-perl gettext
-```
-
-## neovim
-
-```bash
-tar -zxf nvim-linux64-glibc-earlier.tar.gz
-tar -zxf config.tar.gz
-cp -rf  nvim-linux64/* "${XDG_DATA_HOME:-$HOME/.local}"
-cp -rf config   "${XDG_DATA_HOME:-$HOME/.local/share}"/.config
-```
-
-### vim-plug
-
-```bash
-mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload
-cp plug.vim "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload
-```
-
-### Nodejs
-
-```bash
-tar -xf node-v16.18.1-linux-x64.tar.xz
-cp -rf node-v16.18.1-linux-x64/bin "${XDG_DATA_HOME:-$HOME/.local}"
-cp -rf node-v16.18.1-linux-x64/lib "${XDG_DATA_HOME:-$HOME/.local}"
-cp -rf node-v16.18.1-linux-x64/share "${XDG_DATA_HOME:-$HOME/.local}"
-```
-
-### Python
-
-Python 的 neovim 组件
-
-```bash
-pip3 install pynvim --user
-```
-
-### copy-plugin
-
-```bash
-mkdir -p "${XDG_DATA_HOME:-$HOME}"
-cp -rf config  "${XDG_DATA_HOME:-$HOME}"/.config
-cd "${XDG_DATA_HOME:-$HOME}"/.config/
-
-## coc-extension
-cd "${XDG_DATA_HOME:-$HOME}"/.config/coc/extensions
-./package.sh unzip
-rm -f *.tar.gz
-
-## vim-plugin
-cd "${XDG_DATA_HOME:-$HOME}"/.config/nvim/plugged/
-./package.sh upzip
-rm -f *.tar.gz
-```
-
-### gtags
+## gtags
 
 ```bash
 ## https://ftp.gnu.org/pub/gnu/global/
@@ -123,7 +11,7 @@ cd global-6.6.8
 make install
 ```
 
-### ctags
+## ctags
 
 ```bash
 ##https://github.com/universal-ctags/ctags
@@ -134,7 +22,7 @@ cd ctags
 make install
 ```
 
-### ripgrep
+## ripgrep
 
 用于全局搜索的 LeaderF 需要依赖 ripgrep 进行搜索。
 
@@ -150,23 +38,6 @@ tar -zxf git-2.38.1.tar.gz
 cd  git-2.38.1
 ./configure --prefix="${XDG_DATA_HOME:-$HOME/.local}" --without-tcltk
 make install
-```
-
-## ZSH
-
-使用 ZSH 替代 Bash，并安装插件。支持：
-
-- 根据历史自动补全（ctrl+space）
-- 快速目录跳转(`j myfolder` //夸目录跳转）
-- Terminal 美化主题
-
-```bash
-tar -zxf zsh-autosuggestion.tar.gz -C "${XDG_DATA_HOME:-$HOME/.local/share}"
-tar -zxf zsh-syntax-highlighting.tar.gz -C "${XDG_DATA_HOME:-$HOME/.local/share}"
-tar -zxf powerlevel9k.tar.gz -C  "${XDG_DATA_HOME:-$HOME/.local/share}"
-tar -zxf autojump.tar.gz -C "${XDG_DATA_HOME:-$HOME/.local/share}"
-ln -s  "${XDG_DATA_HOME:-$HOME/.local/share}"/autojump/autojump    "${XDG_DATA_HOME:-$HOME/.local}"/bin/autojump
-cp ../configs/zshrc "${XDG_DATA_HOME:-$HOME}"/.zshrc
 ```
 
 ## 关闭 PowerLevel9K 的检查 GIT 更新 Option
--- script/quickstart.sh
+++ script/quickstart.sh
@@ -1,62 +1,66 @@
 #!/bin/bash
-# Replace the url in /etc/apt/sources.list to "mirrors.ustc.edu.cn"
 
-# general
+sudo sed -i 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 sudo apt update
-sudo apt upgrade -y
-sudo apt install git subversion libtool make g++ gcc clang-12 automake tig gettext curl build-essential pkg-config python3-pip ruby universal-ctags ruby-dev cmake build-essential automake autoconf libtool libtool-bin gettext global ripgrep
+sudo apt install -y android-sdk-libsparse-utils autoconf  autogen automake autopoint \
+                    bc bison build-essential cmake curl device-tree-compiler flex \
+                    g++ gcc gettext git global gperf intltool jq lib32stdc++6  \
+                    libcurl4-openssl-dev libcurses-ocaml-dev liblz4-tool libffi-dev \
+                    libdist-zilla-plugin-localemsgfmt-perl liblocale-msgfmt-perl \
+                    libmount-dev libncurses5-dev libtool libtool-bin libxml2-utils \
+                    lsb m4 make pkg-config python3-dev python3-pip ruby ruby-dev tig \
+                    squashfs-tools srecord subversion zip zlib1g-dev zsh autojump \
+                    zsh-theme-powerlevel9k zsh-syntax-highlighting zsh-autosuggestions
 
-# vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# npm install
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-
-# yarn install 
+################################################################################
+# nodejs + yarn 
+################################################################################
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - && sudo apt-get install -y nodejs
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt install -y yarn nodejs
 
-sudo apt update
-sudo apt install -y nodejs yarn 
-
-# npm yarn mirror
-yarn config set registry https://registry.npm.taobao.org --global  
-yarn config set disturl https://npm.taobao.org/dist --global 
-yarn config set sass_binary_site https://npm.taobao.org/mirrors/node-sass --global  
-yarn config set electron_mirror https://npm.taobao.org/mirrors/electron/ --global  
-yarn config set puppeteer_download_host https://npm.taobao.org/mirrors --global  
-yarn config set chromedriver_cdnurl https://npm.taobao.org/mirrors/chromedriver --global  
-yarn config set operadriver_cdnurl https://npm.taobao.org/mirrors/operadriver --global  
-yarn config set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs --global  
-yarn config set selenium_cdnurl https://npm.taobao.org/mirrors/selenium --global  
-yarn config set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector --global
-
-npm config set registry https://registry.npm.taobao.org 
-npm config set disturl https://npm.taobao.org/dist 
-npm config set sass_binary_site https://npm.taobao.org/mirrors/node-sass 
-npm config set electron_mirror https://npm.taobao.org/mirrors/electron 
-npm config set puppeteer_download_host https://npm.taobao.org/mirrors 
-npm config set chromedriver_cdnurl https://npm.taobao.org/mirrors/chromedriver 
-npm config set operadriver_cdnurl https://npm.taobao.org/mirrors/operadriver 
-npm config set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs 
-npm config set selenium_cdnurl https://npm.taobao.org/mirrors/selenium 
-npm config set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector 
+npm set registry https://registry.npm.taobao.org
+npm set disturl https://npm.taobao.org/dist
+npm set sass_binary_site https://npm.taobao.org/mirrors/node-sass
+npm set electron_mirror https://npm.taobao.org/mirrors/electron
+npm set puppeteer_download_host https://npm.taobao.org/mirrors
+npm set chromedriver_cdnurl https://npm.taobao.org/mirrors/chromedriver
+npm set operadriver_cdnurl https://npm.taobao.org/mirrors/operadriver
+npm set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs
+npm set selenium_cdnurl https://npm.taobao.org/mirrors/selenium
+npm set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector
+npm set coc.nvim:registry https://registry.npmjs.org/
 npm cache clean --force
 
+yarn config set registry https://registry.npm.taobao.org --global
+yarn config set disturl https://npm.taobao.org/dist --global
+yarn config set sass_binary_site https://npm.taobao.org/mirrors/node-sass --global
+yarn config set electron_mirror https://npm.taobao.org/mirrors/electron/ --global
+yarn config set puppeteer_download_host https://npm.taobao.org/mirrors --global
+yarn config set chromedriver_cdnurl https://npm.taobao.org/mirrors/chromedriver --global
+yarn config set operadriver_cdnurl https://npm.taobao.org/mirrors/operadriver --global
+yarn config set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs --global
+yarn config set selenium_cdnurl https://npm.taobao.org/mirrors/selenium --global
+yarn config set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector --global
+
+################################################################################
+# neovim 
+################################################################################
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt install neovim -y
 sudo npm install -g neovim
 sudo npm install -g bash-language-server
-sudo pip3 install pynvim 
+pip3 install pynvim 
 sudo gem install neovim
 
-echo "********************************************************************************"
-echo ' cd ~/.config/nvim/plugged/coc.nvim/; yarn install'
-echo ' restart neovim, coc plugin will automatically install'
-echo "********************************************************************************"
-nvim -c PlugInstall -c q -c q
-cd ~/.config/nvim/plugged/coc.nvim/; yarn install
-nvim -c CocUpdate
+mkdir -p "${XDG_DATA_HOME:-$HOME/.config/nvim}"
+mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload
 
-
-mkdir -p ~/.config/nvim
-ln -s ~/works/system_config/configs/init.vim ~/.config/nvim/init.vim
-ln -s ~/works/system_config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+cp config/plug.vim "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload
+ln -s $(pwd)/config/.zshrc "${XDG_DATA_HOME:-$HOME}"/.zshrc
+ln -s $(pwd)/config/init.vim "${XDG_DATA_HOME:-$HOME}"/.config/nvim/init.vim
+ln -s $(pwd)/config/coc-settings.json "${XDG_DATA_HOME:-$HOME}"/.config/nvim/coc-settings.json
+chsh $(USER) -s /usr/bin/zsh
