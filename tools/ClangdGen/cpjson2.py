ebf8582c4f4215af40c8bcb091a9849d0c9af577
dllvhaobo <dl_lvhaobo@163.com>
2023-02-26 09:17:31 +0800

    Add something
    and keyword 'REM' is needed only no ticket/issue is associated at most cases.

--- tools/ClangdGen/cpjson2.py
+++ tools/ClangdGen/cpjson2.py
@@ -2,7 +2,6 @@
 
 import os
 import argparse
-from re import sub
 import traceback
 import yaml
 import logging
@@ -13,37 +12,23 @@ logging.basicConfig(level=logging.INFO,
 logger = logging.getLogger(__name__)
 
 '''
-Readme
-## For Develop
-- bob dev 时指定的定义为module
-- module 依赖的定义为submodule
-- src bob下载代码的路径，通过 `bob  query-path` 可查询
-- build bob编译是中间产物的路径，`bob  query-path` 可查询 通常的compile_commands.json保存在此位置
-- yaml recipe文件所在路径
+# Readme
 
 ## Feature
 
+- 说
 - 查找Module的compille_commands.json文件，添加到.clangd
 - 查找submodule的compille_commands.json文件，添加到.clangd
 - 必要时尝试使用bob dev编译submodule，并生产compile_commands.json，添加到.clangd
+
+## For Develop
+- bob dev 时指定的定义为package
+- package 依赖的定义为sub-subpackage
+- src bob下载代码的路径，通过 `bob  query-path` 可查询
+- build bob编译是中间产物的路径，`bob  query-path` 可查询 通常的compile_commands.json保存在此位置
+- yaml recipe文件所在路径
 '''
 
-'''
-build command:
-
-    CONFIG_JNN=1 bob dev  -DCONFIG_JNN=1 -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ei/ei-variant-37W-GP-VW_CHN/phone::bt-phone::tsd-bt-phone-mib3-target
-
-Recipe与SRC的目录关系
-
-Example：
-    存在一个recipes/phone/bt-phone/tsd-bt-phone-mib3.yaml文件，其中存在如下内容
-
-        checkoutSCM:
-            dir: tsd.bt.phone.mib3
-
-		phone/bt-phone/tsd-bt-phone-mib3.yaml 文件名去除yaml规定工程目录名 ./phone/bt-phone/tsd-bt-phone-mib3/.......
-        dir:tsd.bt.phone.mib3 workspace中git clone的目录名
-'''
 
 __CMD_BOB_QUERY_SRC_PATH__ = "CONFIG_JNN=1 bob query-path -f {{src}} -DCONFIG_JNN=1 {module}"
 __CMD_BOB_QUERY_BUILD_PATH__ = "CONFIG_JNN=1 bob query-path -f {{build}} -DCONFIG_JNN=1 {module}"
@@ -61,10 +46,6 @@ If:
 CompileFlags:
   CompilationDatabase: "{build}"
 '''
-#  DEF_PACKAGES = [
-#      'ei/ei-variant-37W-GP-VW_CHN/phone::bt-phone::tsd-bt-phone-mib3-target',
-#      'ei/ei-variant-37W-GP-VW_CHN/phone::phonemanager::tsd-phonemanager-target'
-#  ]
 
 
 def os_popen(cmd_string):
@@ -95,11 +76,9 @@ class Package:
         if self.info is None:
             cmd_string = __CMD_BOB_SHOW__.format(module=self.name)
             self.info = yaml.full_load(os_popen(cmd_string))
-        #  logger.debug(self.info)
         return self.info
 
     def get_depends(self):
-
         if self.depends is None:
             self.depends = self.get_info().get('depends')
         logger.debug("SubModuls: {}".format(self.depends))
@@ -177,22 +156,10 @@ class Cache:
         #  self.recipes = []
         self.cachefile = kawrgs.get('cachefile', "")
         self.content = dict()
-        self.logger = logger.getChild(__class__.__name__)
-        self.error = self.logger.error
-        self.info = self.logger.info
-        self.debug = self.logger.debug
 
     def load(self):
         try:
             self.content = yaml.full_load(open(self.cachefile, 'r').read())
-        except Exception as err:
-            logger.error(str(err))
-            traceback.print_exc()
-
-    def dump_append(self, module_list):
-        try:
-            with open(self.cachefile, 'a') as f:
-                f.write(yaml.dump(module_list))
         except Exception as err:
             logger.error(str(err))
             traceback.print_exc()
@@ -213,11 +180,8 @@ def config():
     parser.add_argument('--packages', type=str, nargs="+", dest='packages',
                         default=__DEF_PACKAGES__, help='指定目标packages')
 
-    parser.add_argument('--selector', type=str, nargs="+", default="phone",
-                        help='指定关注的模块(目录名)，比如"phone","media"等等')
-
     parser.add_argument(
-        '--cachefile', help="cache文件的路径,默认为当前目录的.clangchage", default='.clangcache')
+        '--cachefile', help="cache文件的路径,默认为当前目录的.clangchage", default='.clangdcache')
 
     parser.add_argument('--save-cache', dest='save_cache', default=True,
                         action='store_true', help="保存所有过程信息，减少下次生成的时间")
@@ -247,56 +211,54 @@ class Clangd:
         self.packages = list()
         self.cache = Cache(**kwargs)
 
+    def write_cache(self):
+        os_popen("rm {}".format(self.kwargs.get('cachefile')))
+        dump_dict = {}
+        for pkg in self.packages:
+            dump_dict.update({pkg.name: pkg.dump()})
+
+        self.cache.dump(dump_dict)
+
+    def write_info(self):
+
+        content_all = []
+        for pkg in self.packages:
+            logger.debug("name :{}\n".format(pkg.name))
+            logger.debug("src  :{}\n".format(pkg.src))
+            logger.debug("build:{}\n".format(pkg.build))
+            content_all.append("==========================================n")
+            content_all.append("name :{}\n".format(pkg.name))
+            content_all.append("src  :{}\n".format(pkg.src))
+            content_all.append("build:{}\n".format(pkg.build))
+
+        with open('.clanginfo', 'w') as f:
+            f.writelines(content_all)
+
+    def write_clangd(self):
+        content = []
+        for pkg in self.packages:
+            if pkg.build is not None and pkg.src is not None:
+                if (os.path.exists(os.path.join(os.getcwd(), pkg.build, 'compile_commands.json'))):
+                    content.append(__CLANG_CFG_PATTERN__.format(
+                        src=pkg.src.replace('dev/src/', ".*/"), build=os.path.join(os.getcwd(), pkg.build)))
+
+        with open(self.kwargs.get('clangfile', '.clangd'), 'w') as f:
+            f.write('---'.join(content))
+
     def process(self):
         logger.debug(self.kwargs)
         for pkg_name in self.kwargs.get('packages', __DEF_PACKAGES__):
             pkg = Package(pkg_name,  **self.kwargs)
             pkg.process(recursive=True)
             self.packages.append(pkg)
-            for sub_pkg in pkg.sub_pkgs:
-                self.packages.append(sub_pkg)
-
-    def write_cache(self):
-        os_popen("rm {}".format(self.kwargs.get('cachefile')))
-        dump_dict = {}
-        for pkg in self.packages:
-            dump_dict.update({pkg.name: pkg.dump()})
-            #  for pkg in pkg.sub_pkgs:
-            #  dump_dict.update({pkg.name: pkg.dump()})
-
-        self.cache.dump(dump_dict)
-
-    def write_clangd(self):
-        content = []
-        content_all = []
-        for pkg in self.packages:
-            logger.info("package_name:{}".format(pkg.name))
-            logger.info("         src:{}".format(pkg.src))
-            logger.info("       build:{}".format(pkg.build))
-            content_all.append("package_name:{}\n".format(pkg.name))
-            content_all.append("         src:{}\n".format(pkg.src))
-            content_all.append("       build:{}\n".format(pkg.build))
-            if pkg.build is not None and pkg.src is not None:
-                if (os.path.exists(os.path.join(os.getcwd(), pkg.build, 'compile_commands.json'))):
-                    content.append(__CLANG_CFG_PATTERN__.format(
-                        src=pkg.src.replace('dev/src/', ".*/"), build=os.path.join(os.getcwd(),pkg.build)))
-
-        with open(self.kwargs.get('clangfile', '.clangd'), 'w') as f:
-            f.write('---'.join(content))
-        with open('all_info', 'w') as f:
-            f.writelines(content_all)
+            self.packages.extend(pkg.sub_pkgs)
+            #  for sub_pkg in pkg.sub_pkgs:
+            #      self.packages.append(sub_pkg)
+        self.write_cache()
+        self.write_clangd()
 
 
-def is_submodule(yaml, module_name):
-    path_grp = yaml.split('/')
-    logger.debug(path_grp)
-    if (module_name == path_grp[1]):
-        return True
-    else:
-        return False
-
-
-def test():
+def main_entry():
     args = config()
     logger.info(args)
     try:
@@ -305,14 +267,10 @@ def test():
         args.update(cache.content)
         clang = Clangd(**args)
         clang.process()
-        clang.write_cache()
-        clang.write_clangd()
     except Exception as e:
         logger.error(str(e))
         traceback.print_exc()
 
 
 if __name__ == "__main__":
-    #  main_entry()
-    #  print(test())
-    test()
+    main_entry()
