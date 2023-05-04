6c9338cc1514bd8968671a130459fbfe70e169b3
dllvhaobo <dl_lvhaobo@163.com>
2023-05-04 21:03:01 +0800

    Linux quickstart install script

--- config/.zshrc
+++ config/.zshrc
@@ -1,7 +1,7 @@
 # Set up the prompt
 
 export TERM="xterm-256color"
-fpath+=("${XDG_DATA_HOME:-$HOME/works/pure}")
+fpath+=($HOME/.zsh/pure)
 autoload -Uz promptinit
 promptinit
 # prompt adam1
@@ -73,8 +73,8 @@ POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=( time dir vcs newline)
 # POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(time context dir vcs)
 # POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status ssh root_indicator background_jobs history time)
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    ZSH_HOME="/usr/share"
-    # ZSH_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+    # ZSH_HOME="/usr/share"
+    ZSH_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
     [ -f "${ZSH_HOME}/zsh-autosuggestions/zsh-autosuggestions.zsh" ] && . ${ZSH_HOME}/zsh-autosuggestions/zsh-autosuggestions.zsh
     [ -f "${ZSH_HOME}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ] && . ${ZSH_HOME}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
     [ -f "${ZSH_HOME}/autojump/autojump.zsh" ] && . ${ZSH_HOME}/autojump/autojump.zsh
--- script/quickstart.sh
+++ script/quickstart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo sed -i 's/archive.ubuntu.com/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+sudo sed -i 's/http:\/\/archive.ubuntu.com/http:\/\/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
+sudo sed -i 's/http:\/\/security.ubuntu.com/http:\/\/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 sudo apt update
 sudo apt install -y android-sdk-libsparse-utils autoconf  autogen automake autopoint \
                     bc bison build-essential cmake curl device-tree-compiler flex \
@@ -10,63 +11,61 @@ sudo apt install -y android-sdk-libsparse-utils autoconf  autogen automake autop
                     libmount-dev libncurses5-dev libtool libtool-bin libxml2-utils \
                     lsb m4 make pkg-config python3-dev python3-pip ruby ruby-dev tig \
                     squashfs-tools srecord subversion zip zlib1g-dev zsh autojump \
-                    zsh-theme-powerlevel9k zsh-syntax-highlighting zsh-autosuggestions \
-                    ripgrep
+                    zsh-syntax-highlighting zsh-autosuggestions ripgrep
 
+ln -s $(pwd)/config/.zshrc "${XDG_DATA_HOME:-$HOME}"/.zshrc
+chsh ${USER} -s /usr/bin/zsh
 
 ################################################################################
 # nodejs + yarn 
 ################################################################################
-curl -fsSL https://deb.nodesource.com/setup_18.x |  \
-    sudo -E bash - && sudo apt-get install -y nodejs
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - && sudo apt-get install -y nodejs
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg \
-    | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
-
-echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | \
-    sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt install -y yarn nodejs
-
-npm set registry https://registry.npm.taobao.org
-npm set disturl https://npm.taobao.org/dist
-npm set sass_binary_site https://npm.taobao.org/mirrors/node-sass
-npm set electron_mirror https://npm.taobao.org/mirrors/electron
-npm set puppeteer_download_host https://npm.taobao.org/mirrors
-npm set chromedriver_cdnurl https://npm.taobao.org/mirrors/chromedriver
-npm set operadriver_cdnurl https://npm.taobao.org/mirrors/operadriver
-npm set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs
-npm set selenium_cdnurl https://npm.taobao.org/mirrors/selenium
-npm set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector
-npm set coc.nvim:registry https://registry.npmjs.org/
-npm cache clean --force
-
-yarn config set registry https://registry.npm.taobao.org --global
-yarn config set disturl https://npm.taobao.org/dist --global
-yarn config set sass_binary_site https://npm.taobao.org/mirrors/node-sass --global
-yarn config set electron_mirror https://npm.taobao.org/mirrors/electron/ --global
-yarn config set puppeteer_download_host https://npm.taobao.org/mirrors --global
-yarn config set chromedriver_cdnurl https://npm.taobao.org/mirrors/chromedriver --global
-yarn config set operadriver_cdnurl https://npm.taobao.org/mirrors/operadriver --global
-yarn config set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs --global
-yarn config set selenium_cdnurl https://npm.taobao.org/mirrors/selenium --global
-yarn config set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector --global
+yarn config set registry https://registry.npmmirror.com
+npm config set registry https://registry.npmmirror.com
 
 ################################################################################
-# neovim 
+# npm installation 
+################################################################################
+npm install neovim
+npm install bash-language-server
+sudo gem install neovim
+
+################################################################################
+# neovim
 ################################################################################
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt install neovim -y
-sudo npm install -g neovim
-sudo npm install -g bash-language-server
-pip3 install pynvim 
-sudo gem install neovim
-
 mkdir -p "${XDG_DATA_HOME:-$HOME/.config/nvim}"
-mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload
+ln -s $(pwd)/working-in-wsl/config/init.vim "${XDG_DATA_HOME:-$HOME}"/.config/nvim/init.vim
+ln -s $(pwd)/working-in-wsl/config/coc-settings.json "${XDG_DATA_HOME:-$HOME}"/.config/nvim/coc-settings.json
 
-cp config/plug.vim "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload
-ln -s $(pwd)/config/.zshrc "${XDG_DATA_HOME:-$HOME}"/.zshrc
-ln -s $(pwd)/config/init.vim "${XDG_DATA_HOME:-$HOME}"/.config/nvim/init.vim
-ln -s $(pwd)/config/coc-settings.json "${XDG_DATA_HOME:-$HOME}"/.config/nvim/coc-settings.json
-chsh $(USER) -s /usr/bin/zsh
+################################################################################
+# pynvim 
+################################################################################
+pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple 
+pip3 install --upgrade pip  
+pip3 install pynvim 
+
+################################################################################
+# vim-plug 
+################################################################################
+mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload
+cp $(pwd)/working-in-wsl/config/plug.vim "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload
+
+################################################################################
+# fzf:  https://github.com/junegunn/fzf.git
+################################################################################
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+################################################################################
+# Theme pure:https://github.com/sindresorhus/pure.git
+################################################################################
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+
